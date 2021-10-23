9d3e2ba36db9d53bee6d85320b1b1bdfeb1bced6
robertm39 <robertpmorton39@gmail.com>
2021-10-23 15:16:25 -0400

    started

---
+++ sphere_gd.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 23 14:42:42 2021
+
+@author: rober
+"""
+
+import numpy as np
+
+def get_random_normal_vector(dimension, min_norm=1e-5):
+    """
+    Return a random normal vector with the given dimension.
+    
+    Parameters:
+        dimension: int
+            The dimension of the vector to return.
+        min_norm: double
+            The minimum norm of the unnormalize vector
+            (to avoid dividing by very small numbers)
+    
+    Return:
+        A random unit vector with the given dimension
+    """
+    
+    while True:
+        # Get a random vector
+        vector = np.random.random_sample((dimension)) - 0.5
+        
+        # Normalize it to make calculations simpler
+        norm = np.linalg.norm(vector)
+        
+        # Get a new vector if the first was too small
+        # to avoid dividing by a very small number
+        if norm < min_norm:
+            continue
+        
+        return vector / norm
+
+def get_orthonormal_vector(vectors, min_norm=1e-8):
+    """
+    Return a random normal vector orthogonal to all the given vectors.
+    
+    Parameters:
+        vectors: list[numpy vector]
+            The vectors for the new vector to be orthogonal to.
+        min_norm: double
+            The minimum norm of the orthogonalized vector before normalizing
+            (to avoid dividing by very small numbers)
+    """
+    dimension = vectors[0].shape[0]
+    
+    while True:
+        # Get a random vector
+        vector = get_random_normal_vector(dimension)
+        
+        # Make it normal to all the previous vectors
+        for v in vectors:
+            vector -= vector * (np.dot(vector, v))
+        
+        norm = np.linalg.norm(vector)
+        
+        if norm < min_norm:
+            continue
+        
+        return vector / norm
+
+def get_orthonormal_basis(x):
+    """
+    Return an orthonormal basis for the space normal to x.
+    
+    Parameters:
+        x: numpy array
+            The point for the space to be normal to.
+    
+    Return:
+        A matrix containing the basis vectors as columns.
+    """
+    
+    vectors = [x]
+    length = x.shape[0]
+    
+    # Add vectors to the orthonormal basis
+    for _ in range(length-1):
+        # Get another basis vector
+        vector = get_orthonormal_vector(vectors)
+        vectors.append(vector)
+    
+    return np.stack(vectors[1:], axis=1)
+
+def spherical_gd_step(x, gradient, alpha, max_step):
+    """
+    Return x after one step of gradient descent,
+    with x constrained to be on the surface of a unit sphere (have norm 1)
+
+    Parameters:
+        x: numpy array
+            The point to update.
+        gradient: numpy array
+            The partial derivatives of the loss with respect to x.
+        alpha: double
+            The learning rate.
+        max_step: double
+            The maximum step angle.
+
+    Return:
+        The updated position of x.
+    """
+    
+    # First, get an orthonormal basis B for the plane normal to x
+    basis = get_orthonormal_basis(x)
+    
+    # Then calculate the dot product of the gradient with each basis vector
+    basis_grad = gradient @ basis # I can do this in one operation
+    
+    # Get the norm of the basis gradient
+    # both to control step size and to normalize
+    step_norm = np.linalg.norm(basis_grad)
+    
+    # The point on the equator of the sphere to step towards
+    step_target = basis_grad / (-step_norm)
+    
+    # The angle to step by
+    angle = min(alpha * step_norm, max_step)
+    
+    return np.cos(angle) * x + np.sin(angle) * step_target
---
+++ sphere_gd_test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 23 15:15:06 2021
+
+@author: rober
+"""
+
+def sphere_gd_test_1():
+    pass
+
+def main():
+    sphere_gd_test_1()
+
+if __name__ == '__main__':
+    main()
